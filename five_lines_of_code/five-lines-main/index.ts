3bfc5a2ec2251088a0bbc9eaef30fbd2862a14c1
DESKTOP-5SQ819M\Happiny <crongcrong222@gmail.com>
2023-10-22 22:38:21 +0900

    [example 3.26] - 위반사항 : if 문은 함수의 시작에만 배치, 다섯 줄 제한 - 해결방법 : 함수 분해 - 적용 리팩터링 패턴 : 메서드 추출

--- five_lines_of_code/five-lines-main/index.ts
+++ five_lines_of_code/five-lines-main/index.ts
@@ -89,15 +89,19 @@ function update() {
 function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
-    if (current === Input.LEFT)
-      moveHorizontal(-1);
-    else if (current === Input.RIGHT)
-      moveHorizontal(1);
-    else if (current === Input.UP)
-      moveVertical(-1);
-    else if (current === Input.DOWN)
-      moveVertical(1);
+    handleInput(current);
   }
+}
+
+function handleInput(input: Input) {
+  if (input === Input.LEFT)
+    moveHorizontal(-1);
+  else if (input === Input.RIGHT)
+    moveHorizontal(1);
+  else if (input === Input.UP)
+    moveVertical(-1);
+  else if (input === Input.DOWN)
+    moveVertical(1);
 }
 
 function updateMap() {
@@ -129,7 +133,7 @@ function draw() {
 
   // Draw map
   drawMap(g);
- 
+
   // Draw player
   drawPlayer(g);
 }
